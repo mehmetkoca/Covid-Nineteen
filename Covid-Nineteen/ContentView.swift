e7b9489508ee56a2b32288304f930e8589a3d861
Mehmet Koca <mehmetkoca@outlook.com>
2020-04-08 14:47:05 +0300

    Initial Commit

---
+++ Covid-Nineteen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-Nineteen
+//
+//  Created by Mehmet Koca on 8.04.2020.
+//  Copyright © 2020 Mehmet Koca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
